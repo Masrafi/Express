f15b4f6012e7d917067729bca48982d30d316bcd
Masrafi Anam <masrafianam@gmail.com>
2025-07-23 17:51:53 +0600

    feat: express details

--- index.js
+++ index.js
@@ -1,19 +1,75 @@
-const express = require('express');
+const express = require('express')
+const app = express()
 
-const app = express();
+// express/json
+//app.use(express.json());
+//
+/// json
+// app.post('/', (req , res) => {
+//     console.log(req.body);
+//     console.log(typeof req.body);
+//     console.log(req.body.name);
+//     res.send("This is masrafi anar. return put request");
+// });
 
-app.get('/', (req, res) => {
-    res.send("This is home page");
+//express/raw
+//app.use(express.raw());
+//
+//raw
+// app.post('/', (req , res) => {
+//     console.log(req.body.toString());
+//     res.send("This is masrafi anar. return put request");
+// });
+
+//express/text
+//app.use(express.text());
+//
+/// text
+// app.post('/', (req , res) => {
+//     console.log(req.body);
+//     res.send("This is masrafi anar. return put request");
+// });
+
+//express/urlencoded
+//app.use(express.urlencoded());
+//
+/// url encoded
+// app.get('/', (req, res) => {
+//     res.send('This is home page');
+// });
+// app.post('/', (req , res) => {
+//     console.log(req.body);
+//     res.send("This is masrafi anar. return put request");
+// });
+
+//express/static
+// app.use(express.static(__dirname + '/public/', {
+//     index: 'home.html'
+// }));
+/// 
+// app.get('/', (req, res) => {
+//     res.send('This is home page');
+// });
+// app.post('/', (req , res) => {
+//     console.log(req.body);
+//     res.send("This is masrafi anar. return put request");
+// });
+
+//express/router
+const router = express.Router({caseSensitive: false});
+
+app.use(router);
+
+router.get('/about', (req, res) => {
+    res.send('This is home page');
 });
-
-app.post('/', (req, res) => {
-    res.send("This is home page with post request");
-});
-
-app.put('/call', (req , res) => {
+router.post('/', (req , res) => {
+    console.log(req.body);
     res.send("This is masrafi anar. return put request");
 });
 
+
+
 app.listen(3000, () => {
-    console.log('listening on port 300');
+    console.log('listening on port 3000');
 });
