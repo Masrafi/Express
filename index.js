d9516a36397d0918ce93f94d3cf6e601443b01f6
Masrafi Anam <masrafianam@gmail.com>
2025-07-30 17:24:53 +0600

    feat: application object

--- index.js
+++ index.js
@@ -1,19 +1,62 @@
 const express = require('express');
+//const handle = require('./handle');
+
 
 const app = express();
+const admin = express();
 
-app.get('/', (req, res) => {
-    res.send("This is home page");
+//app.locals.title = 'My App';
+//app.get('/', handle);
+
+// admin.on('mount', (parent) => {
+//   console.log('Admin Mounted')
+//   console.log(parent) // refers to the parent app
+// })
+
+admin.get('/dashboard', (req, res)=>{
+    console.log(admin.mountpath);
+    res.send('Welcome to admin dashboard');
 });
 
-app.post('/', (req, res) => {
-    res.send("This is home page with post request");
+app.get('/', (req, res)=>{
+    res.send('Welcome to application home');
 });
 
-app.put('/call', (req , res) => {
-    res.send("This is masrafi anar. return put request");
+app.all('/', (req, res)=>{
+    res.send('Welcome to application home from all');
 });
+
+app.use('/admin', admin);
+
+
+//params
+app.param('id', (req, res, next, id) => {
+    const user = {
+        userId: id,
+        name: 'Bangladesh'
+    };
+    req.userDetails = user;
+    next();
+})
+
+app.get('/user/:id', (req, res)=>{
+    console.log(req.userDetails);
+    res.send('Welcome to application home');
+});
+//params end
+
+//route
+app.route('/about/mission')
+    .get((req, res)=>{
+        res.send('Welcome to application home');
+    })
+    .post((req, res)=>{
+        res.send('Welcome to application home post');
+    })
+    .put((req, res)=>{
+        res.send('Welcome to application home put');
+    });
 
 app.listen(3000, () => {
-    console.log('listening on port 300');
+    console.log('listening on port 3000');
 });
